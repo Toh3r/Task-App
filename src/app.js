01918dee115ae494c4328645197b30077790ea4d
DESKTOP-43LG3V1\dant <dan.t@live.ie>
2020-10-31 20:35:20 +0000

    Refactor: Promise chaining to async/await in endpoints

--- src/app.js
+++ src/app.js
@@ -11,73 +11,82 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // Get all users
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
-        res.send(users);     
-    }).catch((e) => {
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (e) {
         res.status(500).send();
-    })
+    }
 });
 
 // Get a user using dynamic endpoint
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     // Contains route params
     const _id = req.params.id;
 
-    User.findById(_id).then((user) => {
+    try {
+        const user = await User.findById(_id);
         if (!user) {
             return res.status(404).send();
         }
-        res.send(user);     
-    }).catch((e) => {
+        res.send(user);
+    } catch (e) {
         res.status(500).send();
-    })
+    }
 });
 
 // Get all tasks
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
-        res.send(tasks);     
-    }).catch((e) => {
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.send(tasks);
+    } catch (e) {
         res.status(500).send();
-    })
+    }
 });
 
 // Get a task using dynamic endpoint
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     // Contains route params
     const _id = req.params.id;
 
-    Task.findById(_id).then((task) => {
+    try {
+        const task = await Task.findById(_id);
         if (!task) {
             return res.status(404).send();
         }
-        res.send(task);     
-    }).catch((e) => {
+        res.send(task);
+    } catch (error) {
         res.status(500).send();
-    })
+    }
 });
 
 // Save a User to the DB
-app.post('/user', (req, res) => {    
+app.post('/user', async (req, res) => {    
     const user = new User(req.body);
-    user.save().then(() => {
-        res.status(201).send(user)
-    }).catch((e) => {
+
+    try {
+        await user.save();
+        res.status(201).send(user);
+    } catch (e) {
         res.status(400).send(e);
-    });
+    }
 });
 
 // Save a Task to the DB
-app.post('/task', (req, res) => {    
+app.post('/task', async (req, res) => {    
     const task = new Task(req.body);
-    task.save().then(() => {
-        res.status(201).send(task)
-    }).catch((e) => {
+
+    try {
+        await task.save();
+        res.status(201).send(task);
+    } catch (e) {
         res.status(400).send(e);
-    });
+    }
 });
 
+// Listen on port whatever
 app.listen(port, () => {
     console.log(`Port is up on ${port}...`);
 });
