da868da2e3e9bdf4f0b053fb69bac4dcc53474eb
DESKTOP-43LG3V1\dant <dan.t@live.ie>
2020-11-01 20:42:28 +0000

    Add: Delete tasks on user delete

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./task')
 
 // Create a user schema using validation provided by mongoose and
 // validation created using validator npm
@@ -111,6 +112,13 @@ userSchema.pre('save', async function(next) {
     }
 
     // On function complete
+    next();
+});
+
+// Delete Users tasks, when user is removed
+userSchema.pre('remove', async function (next) {
+    const user = this;
+    await Task.deleteMany({ owner: user._id });
     next();
 });
 
